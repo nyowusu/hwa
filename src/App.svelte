d0ff04151088c9b641b543c776a88dd535a24556
nyowusu <yaw.owusu@hotmail.com>
2020-05-11 22:16:52 +0100

    html setup complete

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,21 @@
-<h1>Test</h1>
+<script>
+  import HowTo from "./components/HowTo.svelte";
+  import Timer from "./components/Timer.svelte";
+</script>
+
+<h1>Handwashing App</h1>
+
+<Timer />
+<HowTo />
+
+<h3>
+    <a href="https://www.who.int/gpsc/clean_hands_protection/en/">Picture source</a>
+    <a href="https://freesound.org/people/metrostock99/sounds/345086/">Sound source</a>
+</h3>
+
+
+<style>
+    h1, h3 {
+        text-align: center;
+    }
+</style>
