e51267ec70236f4bc928d17b8385b57c693a76d6
nyowusu <yaw.owusu@hotmail.com>
2020-05-11 21:30:25 +0100

    tested blurprint grid

---
+++ src/BluePrintGrid.svelte
@@ -0,0 +1,87 @@
+<div bp="grid 4" class="c">
+  <div bp="text-center" class="kids">1</div>
+  <div bp="text-center" class="kids">2</div>
+  <div bp="text-center" class="kids">3</div>
+</div>
+
+<div bp="grid" class="c">
+  <div bp="4 text-center" class="kids r">1</div>
+  <div bp="8 text-center" class="kids l">2</div>
+</div>
+
+<div bp="grid" class="c">
+  <div bp="6 text-center" class="kids">6</div>
+  <div bp="6 text-center" class="kids">6</div>
+  <div bp="4 text-center" class="kids">4</div>
+  <div bp="4 text-center" class="kids">4</div>
+  <div bp="4 text-center" class="kids">4</div>
+</div>
+
+<div bp="grid 6@md 3@lg" class="c">
+  <div class="kids">1</div>
+  <div class="kids">2</div>
+  <div class="kids">3</div>
+  <div class="kids">4</div>
+</div>
+
+<div bp="grid">
+  <div bp="6 4@md" class="kids">1</div>
+  <div bp="6 8@md" class="kids">2</div>
+</div>
+
+<div bp="grid 3" class="c">
+  <div class="kids">1</div>
+  <div class="kids">2</div>
+  <div class="kids">3</div>
+  <div>
+    Bacon ipsum dolor amet alcatra landjaeger hamburger sausage, pork chop tail shank turducken beef shankle t-bone
+    frankfurter.
+  </div>
+</div>
+
+<div bp="grid 3 vertical-center" class="c">
+  <div class="kids">1</div>
+  <div class="kids">2</div>
+  <div class="kids">3</div>
+  <div>
+    Bacon ipsum dolor amet alcatra landjaeger hamburger sausage, pork chop tail shank turducken beef shankle t-bone
+    frankfurter.
+  </div>
+</div>
+
+<div bp="grid 3 vertical-start" class="c">
+  <div class="kids">1</div>
+  <div class="kids">2</div>
+  <div class="kids">3</div>
+  <div>
+    Bacon ipsum dolor amet alcatra landjaeger hamburger sausage, pork chop tail shank turducken beef shankle t-bone
+    frankfurter.
+  </div>
+</div>
+
+<div bp="grid 3 vertical-end" class="c">
+  <div class="kids">1</div>
+  <div class="kids">2</div>
+  <div class="kids">3</div>
+  <div>
+    Bacon ipsum dolor amet alcatra landjaeger hamburger sausage, pork chop tail shank turducken beef shankle t-bone
+    frankfurter.
+  </div>
+</div>
+
+<style>
+  .kids {
+    background-color: steelblue;
+    color: white;
+    padding: 35px 40px;
+    font-size: x-large;
+  }
+
+  div:nth-child(2n) {
+    background-color: red;
+  }
+
+  div.c {
+    margin: 20px 0;
+  }
+</style>
